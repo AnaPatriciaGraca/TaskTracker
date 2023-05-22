ce2aaa2ed8a4e13bddc7d2a2ca35bf0cd93bddb6
Ana Patrícia <78435927+AnaPatriciaGraca@users.noreply.github.com>
2023-05-22 17:50:23 +0100

    handle tasks

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 //rafce
-const Header = ({ title }) => {
-    const onClick = (e) => {
-        console.log('Click')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' handleClick={onClick} />
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} handleClick={onAdd} />
         </header>
     )
 }
